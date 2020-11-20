95cc63ae57eb7be2c236a85b115b30ac7f18e4ab
SamuAlmeida <samuel.almeida@bandtec.com.br>
2020-11-20 11:07:44 -0300

    tirando api dos sensores

---
+++ meusite/projeto-site/conexao.js
@@ -0,0 +1,11 @@
+const Sequelize =  require('sequelize');
+const sequelize = new Sequelize('site','root','bandtec',{
+    host: 'localhost',
+    dialect: 'mysql'
+});
+
+sequelize.authenticate().then(function(){
+    console.log('Conectado com sucesso!');
+}).catch(function(erro){
+    console.log('Falha ao conectar-se:'+erro);
+});
