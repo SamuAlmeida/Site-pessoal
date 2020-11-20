8a49a36aa467d070babb49895e09a99ddd11ed7c
SamuAlmeida <samuel.almeida@bandtec.com.br>
2020-11-20 14:03:22 -0300

    adicionando e chumbando o código com api nova

---
+++ meusite/projeto-site/models/usuario.js
@@ -0,0 +1,40 @@
+	'use strict';
+
+/* 
+lista e explicação dos Datatypes:
+https://codewithhugo.com/sequelize-data-types-a-practical-guide/
+*/
+
+module.exports = (sequelize, DataTypes) => {
+    let Usuario = sequelize.define('usuario',{
+		id: {
+			field: 'id_usuario',
+			type: DataTypes.INTEGER,
+			primaryKey: true,
+			autoIncrement: true
+		},		
+		nome: {
+			field: 'nome_usuario',
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		email: {
+			field: 'email_usuario',
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		senha: {
+			field: 'senha_usuario',
+			type: DataTypes.STRING,
+			allowNull: false
+		}
+	}, 
+	{
+		tableName: 'usuario', 
+		freezeTableName: true, 
+		underscored: true,
+		timestamps: false,
+	});
+
+    return Usuario;
+};
